459320e14a8dae91fd3e8c5ee6d265240c4e893e
Esma Koçak <esma.kocaakk20@gmail.com>
2024-08-08 19:18:38 +0300

    Drinks menu app implementation

---
+++ UITutorials/DrinkMenuApp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  UITutorials
+//
+//  Created by Esma Koçak on 8.08.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
